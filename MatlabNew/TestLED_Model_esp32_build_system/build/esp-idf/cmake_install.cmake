1a5e3142ecda98c958f3a4dd48983a25e6f2a393
20603william <williamlin821@gmail.com>
2025-02-17 09:38:15 -0700

    committing and pulling

---
+++ MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/cmake_install.cmake
@@ -0,0 +1,88 @@
+# Install script for directory: C:/Users/athar/Desktop/W/targets/esp32/setup/esp_idf
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/waijung2_esp32")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/Users/athar/Desktop/W/targets/esp32/setup/.espressif/tools/riscv32-esp-elf/esp-2021r2-8.4.0/riscv32-esp-elf/bin/riscv32-esp-elf-objdump.exe")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_ringbuf/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/efuse/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_timer/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_ipc/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/driver/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_pm/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/mbedtls/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/bootloader/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esptool_py/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/partition_table/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/app_update/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/bootloader_support/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/spi_flash/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/nvs_flash/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/pthread/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_gdbstub/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/espcoredump/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_phy/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_system/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/riscv/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp32c3/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_common/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_rom/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/hal/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/vfs/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_eth/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/tcpip_adapter/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_netif/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_event/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/wpa_supplicant/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_wifi/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/ieee802154/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/console/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/openthread/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/lwip/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/log/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/heap/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/soc/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/esp_hw_support/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/app_trace/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/freertos/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/newlib/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/cxx/cmake_install.cmake")
+  include("C:/Users/athar/Desktop/SideProjects/HACKED-2025-Integrating-Factor/MatlabNew/TestLED_Model_esp32_build_system/build/esp-idf/main/cmake_install.cmake")
+
+endif()
+
